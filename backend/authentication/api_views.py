e0f4b33ba21fe7c6af68d892d09643bb209fb07f
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-11-27 08:35:42 +0300

    #4 Add user logout API view

--- backend/authentication/api_views.py
+++ backend/authentication/api_views.py
@@ -1,19 +1,12 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
 from rest_framework.views import APIView
 from rest_framework.responses import Response
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
-from django.contrib.auth import login
-from django.contrib.auth forms import AuthenticationForm
+from .api_views import UserSerializer
 
 
-# Login View
-class LoginView(APIView):
+# Logout View
+class LogoutView(APIView):
     def post(self, request):
-        form = AuthenticationForm(request.data)
-        if form.is_valid():
-            user = form.get_user()
-            authenticate(request, user)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        return Response(form.errors, 200)
+        logout(request)
+        return Response(status=204)
