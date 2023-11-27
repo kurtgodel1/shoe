9224bc4c3bee14926666be22d050456f504a1ab9
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-11-27 19:48:01 +0300

    #4

--- backend/auth_app/models.py
+++ backend/auth_app/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Add any additional fields here
+    # Example: Adding a bio field
+    bio = models.TextField(null=True, blank=True)
+
+    # You can also override methods or add new methods if needed
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'auth_app.User'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
