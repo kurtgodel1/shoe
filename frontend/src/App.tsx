ac911114a6e61850fed7e0719464fc4a541b93ae
Ali Osman Yılmaz <aliosmanyilmaz@ogr.iu.edu.tr>
2023-12-25 23:08:47 +0300

    Signing jobs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import LoginForm from './components/auth_components/LoginForm';
-import RegistrationForm from './components/auth_components/RegistrationForm';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import MainLayout from './components/layout/MainLayout';
 import HomePage from './pages/HomePage';
@@ -10,31 +8,37 @@ import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import ProductListingPage from './pages/ProductListingPage';
+import SignInSide from './components/auth_components/SigninSide';
+import SignUpSide from './components/auth_components/SignUpSide';
 
 // import './App.css
 
 function App() {
 
   return (
-      <Router>
+    <Router>
+    <Routes>
+      <Route path="/login" element={<SignInSide />} />
+      <Route path="/register" element={<SignUpSide />} />
+      <Route path="/*" element={
         <MainLayout>
           <Container style={{ width: '100%' }}>
-              <Box my={4} style={{ width: '100%' }}>
-                  <Routes>
-                      <Route path="/" element={<HomePage />} />
-                      <Route path="/products" element={<ProductListingPage />} />
-                      <Route path="/product/:productId" element={<ProductDetailPage />} />
-                      <Route path="/cart" element= {<CartPage />} />
-                      <Route path="/checkout" element={<CheckoutPage />} />
-                      <Route path="/login" element={<LoginForm />} />
-                      <Route path="/register" element={<RegistrationForm />} />
-                      {/* More routes */}
-                  </Routes>
-              </Box>
+            <Box my={4} style={{ width: '100%' }}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<ProductListingPage />} />
+                <Route path="/product/:productId" element={<ProductDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                {/* More routes */}
+              </Routes>
+            </Box>
           </Container>
         </MainLayout>
-          <SpeedInsights />
-      </Router>
+      } />
+    </Routes>
+    <SpeedInsights />
+  </Router>
   );
 }
 
---
+++ frontend/src/components/auth_components/SignUpSide.tsx
@@ -0,0 +1,181 @@
+import { useState, FormEvent } from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import axios from 'axios';
+import config from '../../config';
+import { useNavigate } from 'react-router-dom';
+import { Typography, TypographyProps } from '@mui/material';
+import { TextField, Button} from '@mui/material';
+
+
+
+function Copyright(props: TypographyProps) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function SignUpSide() {
+    const [username, setUsername] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string>('');
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    const validateForm = (): boolean => {
+        if (username.trim() === '') {
+            setError('Username is required.');
+            return false;
+        }
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            setError('A valid email is required.');
+            return false;
+        }
+        if (password.length < 6) {
+            setError('Password must be at least 6 characters long.');
+            return false;
+        }
+        setError('');
+        return true;
+    };
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        if (!validateForm()) return;
+        setIsSubmitting(true);
+
+        try {
+            await axios.post(`${config.API_URL}/auth_app/register/`, {
+                username,
+                email,
+                password
+            });
+            navigate('/login');
+            // Handle success (e.g., display a success message, navigate to login page, etc.)
+        } catch (err) {
+            console.error('Registration error:', err);
+            setError('Failed to register. Please check your details.');
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    const handleSignInClick = () => {
+      navigate('/login'); // Navigates to the register page
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign Up
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="email"
+                label="Email Address"
+                type="email"
+                id="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={isSubmitting}
+              >
+                Sign Up
+              </Button>
+              {error && <div style={{ color: 'red' }}>{error}</div>}
+              <Grid container>
+                <Grid item xs>
+                </Grid>
+                <Grid item>
+                <Link href="#" variant="body2" onClick={handleSignInClick}>
+                    {"Have an account? Sign In"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
